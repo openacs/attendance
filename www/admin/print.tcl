16c6158a2cd8b218058f34f64a251f04d201c91b
Hamilton Chua <hamilton.chua@gmail.com>
2005-08-17 10:19:20 +0000

    remove extra columns in the printable attendance sheet

--- www/admin/print.tcl
+++ www/admin/print.tcl
@@ -26,24 +26,20 @@ calendar::item::get -cal_item_id $cal_item_id -array cal_item_info
 
 set message "<p>Attendance for <b>$task_name</b> on $cal_item_info(full_start_date)</p>"
 
+set elements {member_name {label "Member Name"}}
+
+# append elements { medical_needs {label "Medical Needs"}}
+# append elements { special_issues {label "Special Issues"}}
+
+append elements { present {
+		label "Present"
+	    	display_template { <input type=checkbox name=\"user_id\" value=\"@eval_members.user_id@\"  <if @eval_members.present@>checked</if>>  }
+		} }
+
 template::list::create \
     -name eval_members \
     -multirow eval_members \
-    -elements {
-	member_name { 
-		label "Name"
-	}
-        medical_needs {
-		label "Medical Needs"
-        }
-        special_issues {
-		label "Special Issues"
-        }
-	present {
-		label "Present"
-	    	display_template { <input type=checkbox name=\"user_id\" value=\"@eval_members.user_id@\"  <if @eval_members.present@>checked</if>>  }
-	}
-     }
+    -elements $elements
 
 set users [dotlrn_community::list_users $community_id]
 
