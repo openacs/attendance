f250de5000cfff473195671df388a22891e3b7ae
Hamilton Chua <hamilton.chua@gmail.com>
2006-02-27 05:48:50 +0000

    fix typo in attendance.info, remove code that looks for custom fields on another table

--- www/admin/print.tcl
+++ www/admin/print.tcl
@@ -22,25 +22,9 @@ set cal_item_id [db_string "get_cal_id" "select cal_item_id from evaluation_cal_
 
 calendar::item::get -cal_item_id $cal_item_id -array cal_item_info
 
-# set due_date_pretty [lc_time_fmt $due_date "%q %r"]
-
 set message "<p>Attendance for <b>$task_name</b> on $cal_item_info(full_start_date)</p>"
 
 set elements {member_name {label "Member Name"}}
-
-set custom_fields [parameter::get -package_id [apm_package_id_from_key dotlrn-ecommerce] -parameter CustomParticipantFields -default ""]
-
-if { [lsearch $custom_fields allergies] != -1 } {
-    lappend elements medical_needs {
-	label "Medical Needs"
-    }
-}
-
-if { [lsearch $custom_fields special_needs] != -1 } {
-    lappend elements special_issues {
-	label "Special Issues"
-    }
-}
 
 append elements { present {
 		label "Present"
@@ -54,13 +38,9 @@ template::list::create \
 
 set users [dotlrn_community::list_users $community_id]
 
-template::multirow create eval_members user_id member_name medical_needs special_issues present
-
-ns_log Notice " ** $users ** "
+template::multirow create eval_members user_id member_name present
 
 foreach user $users {
-	set medical_needs [db_string "get_medical_issues" "select allergies from person_info where person_id =[ns_set get $user user_id]" -default ""]
-        set special_issues [db_string "get_special_issues" "select special_needs from person_info where person_id = [ns_set get $user user_id]" -default ""]
 	set present [db_0or1row "checkattendance" "select user_id from attendance_cal_item_map where cal_item_id = :cal_item_id and user_id = [ns_set get $user user_id]"]
-	template::multirow append eval_members "[ns_set get $user user_id]" "[ns_set get $user first_names] [ns_set get $user last_name]" "$medical_needs" "$special_issues" "$present"
+	template::multirow append eval_members "[ns_set get $user user_id]" "[ns_set get $user first_names] [ns_set get $user last_name]" "$present"
 }
